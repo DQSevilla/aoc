59bae37f2f9d8d0b5cb0f84ade31b4a1f0c4f373
David Sevilla <sevilladavid83@gmail.com>
2019-12-02 02:59:41 -0500

    Solve AOC 2019 #1-2 with Python and Golang

--- 2019/1/solution.go
+++ 2019/1/solution.go
@@ -17,6 +17,20 @@ func TotalFuelRequirements(module_masses []int) (total int) {
 	return
 }
 
+// RealTotalFuelRequirements solves part two
+func RealTotalFuelRequirements(module_masses []int) (total int) {
+	var prospective_fuel int
+
+	for _, mass := range module_masses {
+		prospective_fuel = mass/3 - 2
+		for prospective_fuel > 0 {
+			total += prospective_fuel
+			prospective_fuel = prospective_fuel/3 - 2
+		}
+	}
+	return
+}
+
 // main driver for AOC 2019 #1
 func main() {
 	var module_masses []int
@@ -42,4 +56,7 @@ func main() {
 
 	fmt.Println("Total fuel requirements for all modules on spacecraft:",
 		TotalFuelRequirements(module_masses))
+	fmt.Println("Total fuel requirements for all modules on spacecraft",
+		"taking into account the fuel's fuel:",
+		RealTotalFuelRequirements(module_masses))
 }
--- 2019/1/solution.py
+++ 2019/1/solution.py
@@ -5,6 +5,21 @@ def total_fuel_requirements(module_masses):
     """
     return sum([mass // 3 - 2 for mass in module_masses])
 
+def real_total_fuel_requirements(module_masses):
+    """
+    Puzzle part two solution
+    """
+    def fuel_of_mass(mass):
+        return mass // 3 - 2
+
+    total = 0
+    for mass in module_masses:
+        prospective_fuel = fuel_of_mass(mass)
+        while prospective_fuel > 0:
+            total += prospective_fuel
+            prospective_fuel = fuel_of_mass(prospective_fuel)
+    return total
+
 def main():
     """
     AOC 2019 Day 1 Driver
@@ -15,6 +30,9 @@ def main():
 
     print('Total fuel requirements for all modules on spacecraft:',
           total_fuel_requirements(module_masses))
+    print('Total fuel requirements for all modules on spacecraft taking ' +
+          'into account the fuel\'s fuel requirements:',
+          real_total_fuel_requirements(module_masses))
 
 if __name__ == '__main__':
     main()
